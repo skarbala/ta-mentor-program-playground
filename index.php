4a9320facd5f473fd761c7e09b8efe11e04800e0
martin.skarbala <skarbala.martin@gmail.com>
2019-02-12 12:25:27 +0100

    updated homepage

--- index.php
+++ index.php
@@ -6,10 +6,41 @@
         integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
         crossorigin="anonymous">
   <meta charset="UTF-8">
+  <link href="https://fonts.googleapis.com/css?family=Exo:300i|Russo+One" rel="stylesheet">
+
+  <style>
+    body {
+      text-align: center;
+      background-color: #f8f9fa;
+    }
+
+    h1 {
+      font-family: 'Russo One', sans-serif;
+      font-size: 8em;
+      text-shadow: 2px 2px #444444;
+
+    }
+
+    h2 {
+      font-family: 'Exo', sans-serif;
+      font-size: 3em;
+
+    }
+    .titles{
+      margin-top: 250px;
+    }
+
+
+  </style>
+
   <title>Title</title>
+
 </head>
 <body>
 <?php include 'partials/navbar.php' ?>
-
+<div class="titles">
+  <h1>Welcome</h1>
+  <h2>to TA mentor program</h2>
+</div>
 </body>
 </html>
